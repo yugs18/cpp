05d1244fc6f1e67c8cd91a097f60b4055775d3fc
yugs18 <yugs1511@gmail.com>
2026-02-12 17:12:56 +0000

    selection-sort

---
+++ strivers-dsa-sheet/sorting/bubble-sort.cpp
@@ -0,0 +1,4 @@
+/* Given an array of N integers, write a program to implement the Bubble Sorting algorithm. */
+#include <iostream>
+using namespace std;
+
---
+++ strivers-dsa-sheet/sorting/selection-sort.cpp
@@ -0,0 +1,29 @@
+/* Given an array of N integers, write a program to implement the Selection sorting algorithm. */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i =0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[i])
+                swap(arr[i], arr[j]);
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    
+    cout << endl;
+    return 0;
+}
