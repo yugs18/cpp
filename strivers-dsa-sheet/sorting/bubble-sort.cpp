257799e243c6c271a8d5afb2f0e7af8795bf61fa
yugs18 <yugs1511@gmail.com>
2026-02-12 18:01:11 +0000

    bubble-sort

--- strivers-dsa-sheet/sorting/bubble-sort.cpp
+++ strivers-dsa-sheet/sorting/bubble-sort.cpp
@@ -1,4 +1,29 @@
 /* Given an array of N integers, write a program to implement the Bubble Sorting algorithm. */
 #include <iostream>
+#include <vector>
 using namespace std;
 
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+    return 0;
+}
--- strivers-dsa-sheet/sorting/selection-sort.cpp
+++ strivers-dsa-sheet/sorting/selection-sort.cpp
@@ -14,10 +14,12 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
+        int min = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[i])
-                swap(arr[i], arr[j]);
+            if (arr[j] < arr[min])
+                min = j;
         }
+        swap(arr[i], arr[min]);
     }
 
     cout << "Sorted array: ";
